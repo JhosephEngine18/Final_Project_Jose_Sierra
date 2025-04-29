5e80731eb031199939a4432f8753d843821e729d
GameDevJ180 <josesielo1805@gmail.com>
2025-04-28 18:46:04 -0600

    First Commit

---
+++ src/main.cpp
@@ -0,0 +1,150 @@
+/*
+Universidad Amerike
+Author: José Sierra
+Final Project
+Date: 28/04/25
+Description: This codes uses contructors with default parameters
+*/
+
+#include <iostream>
+#include <limits>
+using namespace std;
+
+int Decision = 0;
+class Events
+{
+private:
+    int Life = 100;
+    int Ki = 5;
+    int Stamina = 6;
+public:
+
+void RecoverStamina()
+{
+    Stamina = Stamina + 1;
+}
+
+void Dodge()
+{
+    Stamina = Stamina - 2;
+}
+
+void getStamina()
+{
+    cout << "Stamina: " << Stamina << "\n";
+}
+
+void LoseLife(int Value)
+{
+    Life = Life - Value;
+
+    cout << "Life left: " << Life << "\n";
+}
+
+void getLife()
+{
+    cout << "Life: " << Life << "\n";;
+}
+
+void LoseKi(int Value)
+{
+    Ki = Ki - Value;
+}
+
+void getKi()
+{
+    cout << "Ki: " << Ki << "\n";;
+}
+
+void RecoverKi(int Value)
+{
+    Ki = Ki + Value;    
+}
+
+};
+
+
+void text_box(string text)
+{
+    cout << " " << text << "\n"; 
+}
+
+void special_text_box(string text)
+{
+    cout << "|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|\n";
+    cout << " " << text << "\n"; 
+}
+
+
+int main()
+{
+    Events NewEvents;
+    text_box("You are a time patroller whose duty is to prevent bad people from altering the main time line");
+    text_box("they called you to let you know that someone has broken into the sacred timeline in the year 762");
+    text_box("when you arrive at the place you get the surprise that Goku will not only face Freezer in the Namek fight");
+    text_box("his brother Cooler meets him ready to defeat him,");
+    text_box("however, it's not the only thing you notice...");
+    cout << "\n";
+    text_box("You notice that Cooler and Freezer have an unusual increase in power.");
+    text_box("Your duty will be to help Goku defeat Cooler and weaken Freezer enough for Goku to defeat him.");
+    cout << "\n";
+    text_box("But be careful if you make the mistake of not letting history fluctuate as it should, the consequences can be dire......");
+    cin.get();
+    system("cls");
+
+    while (true)
+    {
+        NewEvents.getLife();
+        NewEvents.getKi();
+        NewEvents.getStamina();
+        
+        text_box("Cooler lunges at you looking to connect a blow!!!!!");
+        text_box("What do you want to do?");
+        special_text_box("1-Attack");
+        special_text_box("2-Attack from a distance");
+        special_text_box("3-Dodge");
+        special_text_box("4-Charge Ki");
+
+        cin >> Decision;
+
+        if (cin.fail())
+        {
+            cin.clear();cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            system("cls");
+        }
+        else
+        {
+            break;
+        }
+    }
+        
+    switch (Decision)
+    {
+    case 1:
+        text_box("You also attacked him, exchanging blows with great aggressiveness, causing a great damage to him, but also a little of damage to you.....");
+        NewEvents.LoseLife(15);
+        cin.get();
+        break;
+    case 2:
+
+        text_box("You tried to launch ki blast at Cooler, but that was not enough to make him step back, causing Cooler to punch you in the face, sending you at a bunch of rocks, causing great damage");
+        NewEvents.LoseLife(25);
+        break;
+    case 3:
+        text_box("Cooler tried to punch you, but you dodge it to attack him from behind");
+        NewEvents.Dodge();
+        break;
+    case 4:
+        text_box("You made yourself stronger than before, but same time making Cooler angrier");
+        break;
+    
+    default:
+        
+        break;
+    }
+    
+    
+    return 0;
+    
+}
